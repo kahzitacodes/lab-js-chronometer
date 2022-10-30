7bb3678004f1b7ccf89ced62798d19b800544fd8
Karine Pereira <karinesp89@gmail.com>
2022-10-30 20:18:08 +0100

    done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,66 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+
+    this.currentTime = 0;
+    this.intervalId = null;
+
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+
+    this.intervalId = setInterval(() => {
+
+      this.currentTime = this.currentTime + 1;
+
+      if (!printTimeCallback){
+        return
+      }
+      printTimeCallback();
+
+    }, 1000);
+
   }
 
   getMinutes() {
-    // ... your code goes here
+
+    let minutes = Math.floor(this.currentTime / 60);
+    return minutes;
+
   }
 
   getSeconds() {
-    // ... your code goes here
+
+    let seconds = this.currentTime % 60;
+    return seconds;
+
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    
+    let digitString = value.toString();
+		
+    return digitString.length === 1 ? digitString = '0' + digitString : digitString;
+
   }
 
   stop() {
-    // ... your code goes here
+    
+    clearInterval(this.intervalId);
+
   }
 
   reset() {
-    // ... your code goes here
+
+    this.currentTime = 0;
+
   }
 
   split() {
-    // ... your code goes here
+
+    let seconds = this.computeTwoDigitNumber(this.getSeconds());
+    let minutes = this.computeTwoDigitNumber(this.getMinutes());
+
+    return minutes + ':' + seconds;
+
   }
 }
